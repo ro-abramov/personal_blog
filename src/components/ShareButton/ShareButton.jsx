cf86d552b1ba690143a10db60e1fe1f356125a80
Ruslan Abramov <r.o.abramov@gmail.com>
2019-06-20 17:30:06 +0300

    fix(Sharing) add can-use-dom lib

--- src/components/ShareButton/ShareButton.jsx
+++ src/components/ShareButton/ShareButton.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import styled from 'styled-components'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faShareAlt } from '@fortawesome/free-solid-svg-icons'
+import canUseDom from 'can-use-dom'
 import { colors } from '../../utils/css/mixins'
 
 const ActualShareButton = styled.button`
@@ -14,16 +15,14 @@ const ShareIcon = styled(FontAwesomeIcon)`
     color: ${colors.main};
     font-size: 1rem;
 `
-let url = window.document ? window.document.location.href : ''
-const canonicalElement = window.document
-    ? window.document.querySelector('link[rel=canonical]')
-    : 'https://roabramov.com'
+let url = canUseDom ? window.document.location.href : ''
+const canonicalElement = canUseDom ? window.document.querySelector('link[rel=canonical]') : 'https://roabramov.com'
 if (canonicalElement !== null) {
     url = canonicalElement.href
 }
 
 export function ShareButton({ slug, title }) {
-    if (!navigator || !navigator.share) {
+    if (!canUseDom || !navigator.share) {
         return null
     }
 
