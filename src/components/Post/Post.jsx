b44ca92e9422941cd3f52fbe7cbbccce9e30bde2
Ruslan Abramov <r.o.abramov@gmail.com>
2019-07-03 14:27:23 +0300

    Add authors list component

--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -4,13 +4,15 @@ import { DiscussionEmbed } from 'disqus-react'
 import * as atoms from './Post.atoms'
 import { BackGroundImage } from './components/BackGroundImage'
 import { CenterContent } from '../CenterContent'
+import { AuthorsList } from './components/AuthorsList'
 
-export function Post({ title, img, date, timeToRead, content, slug }) {
+export function Post({ title, img, date, timeToRead, content, slug, authors }) {
     const disqusConfig = {
         url: `https://roabramov.netlify.com${slug}`,
         identifier: slug,
         title: title,
     }
+    const shouldShowAuthors = authors.length > 1 || authors[0].name !== 'Ruslan Abramov'
     return (
         <>
             <BackGroundImage alt={title} fluid={img}>
@@ -18,6 +20,7 @@ export function Post({ title, img, date, timeToRead, content, slug }) {
                     <atoms.PostTitle>{title}</atoms.PostTitle>
                     <atoms.PostInfo>{date}</atoms.PostInfo>
                     <atoms.PostInfo>{timeToRead} min read</atoms.PostInfo>
+                    {shouldShowAuthors ? <AuthorsList authors={authors} /> : null}
                 </atoms.PostInfoContainer>
             </BackGroundImage>
             <CenterContent>
